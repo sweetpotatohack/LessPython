fede19a6615a70fa1d1a44ec1f2a129560a9ed0c
sweetpotatohack <62688852+sweetpotatohack@users.noreply.github.com>
2021-05-13 20:18:35 +0300

    Add files via upload

---
+++ les3.2.py
@@ -0,0 +1,14 @@
+#2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
+# имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы.
+# Реализовать вывод данных о пользователе одной строкой.
+
+ name = input('enter name')
+    surname = input('enter surname')
+    year = int(input('enter year'))
+    city = input('enter city')
+    email = input('enter email')
+    telephone = input('input telephone')
+'''
+def my_func (name, surname, year, city, email, telephone):
+     return ' '.join([name, surname, year, city, email, telephone])
+print(my_func(surname = 'Frolov', name = 'Sergey', year = '1990', city = 'Syzran', email = 'error@mail.ru', telephone = '8-903-300-99-87'))
